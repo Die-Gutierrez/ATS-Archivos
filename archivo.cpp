1b588b6af0f2cbd7d0053f2a81629dc4d8f3e2aa
Die-Gutierrez <dgutierrez@unjbg.edu.pe>
2022-10-12 20:35:24 -0500

    Ingresando el nombre del archivo, y un texto en el archivo

--- archivo.cpp
+++ archivo.cpp
@@ -15,9 +15,9 @@ int main()
 }
 void escribir()
 {
-    string nombre;
+    string nombre, texto;
     ofstream archivo; // Declaramos una variable.
-    
+
     cout<<"Ingrese el nombre del archivo: ";
     fflush(stdin); getline(cin, nombre);
     archivo.open(nombre.c_str(),ios::out); // Abrimos el archivo.
@@ -26,7 +26,9 @@ void escribir()
         cout<<"Programa no funcionando."<<endl;
         exit(0);
     }
-    archivo<<"Hello my name is Diego and have twenty-one."<<endl; /*Comenzamos a escribir dentro de nuestro
+    cout<<"Ingrese un texto: ";
+    fflush(stdin); getline(cin, texto);
+    archivo<<texto<<endl; /*Comenzamos a escribir dentro de nuestro
     archivo texto.txt*/
     archivo.close(); // Cerramos el archivo.
 }
